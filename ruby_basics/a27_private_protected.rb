ad1441528ac974d9722b94c7cb168ce075235ea6
Bruno Gouveia <78083006+brunovcg@users.noreply.github.com>
2022-04-24 17:27:20 -0300

    teste

--- ruby_basics/a27_private_protected.rb
+++ ruby_basics/a27_private_protected.rb
@@ -19,23 +19,28 @@ foo.call_private
 
 puts nil
 
-# ! PROTECTED só pode ser chamado de dentro da classe e de seus filhos
+# ! PROTECTED só pode ser chamado de dentro da classe e de suas
+# ! instancias, consegue chamar via self
 class Foo2
   def call_protected(instance)
     instance.bar
+    # ou
+    self.bar
+    # ou
+    bar
   end
 
   protected
 
   def bar
-    puts 'private method'
+    puts 'protected method'
   end
 end
 
 instance1 = Foo2.new
-
 instance2 = Foo2.new
-
 instance1.call_protected(instance1)
-
+# consigo chamar entre instancias
 instance1.call_protected(instance2)
+
+# intance1.bar => vai dar erro 
